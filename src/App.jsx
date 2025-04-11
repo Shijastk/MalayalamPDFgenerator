1e4404ba455295fc19b1ede47c8c8396b6ad9da5
Shijas TK <Shijasmuhammed573@gmail.com>
2025-04-11 20:51:00 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import PdfGenerator from './page/PdfGenerator.jsx'
+const App = () => {
+  return (
+    <div>
+      <PdfGenerator/>
+    </div>
+  )
+}
+
+export default App
